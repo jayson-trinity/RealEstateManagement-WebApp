1c069bb6dd41bad95821d5b2e1644d29e7bb75f4
Anosike Jayson <kayceeanosike1@gmail.com>
2022-10-20 16:49:58 -0700

    bootstrap fix

--- TRINTY ESTATES/Program.cs
+++ TRINTY ESTATES/Program.cs
@@ -11,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
+
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddDbContext<AuthContext>(options =>  options.UseSqlServer(builder.Configuration.GetConnectionString("AuthContextConnection")));
 
